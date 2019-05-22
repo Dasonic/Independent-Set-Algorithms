179c311da1b8f7c3b9e5579c19aa0ea3763cbaa2
Dasonic <lachlanroberts@hotmail.com>
2019-05-22 13:43:22 +1000

    this file broke lol, don't use

--- s5088947_MIS.cpp
+++ s5088947_MIS.cpp
@@ -2,88 +2,18 @@
 #include <fstream>
 #include <list>
 #include <map>
+#include <iterator>
 
 using namespace std;
 
-class Graph {
-	private:
-		//bool **adj_matrix;
-		//list<int> *adj_list;
-		int num_vertices;
-		map<int, list<int>> test_list;
-	public:
-		Graph(int num_vertices) {
-			this->num_vertices = num_vertices;
-			// Declare adjacency matrix
-			//adj_matrix[num_vertices][num_vertices] = {false};
-			//cout << adj_matrix[300][400] << endl;
-			// Declare adjacency list
-			//adj_list = new list<int>[num_vertices];
-		}
-		void add_edge(int i, int j) {
-			cout << "In add edge" << endl;
-			/*
-			cout << adj_matrix[300][400] << endl;
-			adj_matrix[i][j] = true;
-			adj_matrix[j][i] = true;
-			cout << "after matrix" << endl;
-			*/
-			test_list[i].push_back(j);
-			test_list[j].push_back(i);
-			/*
-			adj_list[i].push_back(j);
-			adj_list[j].push_back(i);
-			cout << "after list" <<endl;
-			*/
-		}
-		/*
-		bool is_edge(int i, int j) {
-			return adj_matrix[i][j];
-		}
-		*/
-		// Finds the vertex with the least amount of edges
-		int find_min_degree() {
-			unsigned int min_degree = 99999999;
-			//tuple min_vertex = make_tuple(-1, -1);
-			int min_vertex = -1;
-			for (int i = 0; i < num_vertices; i++) {
-				long unsigned int list_size = test_list[i].size();
-				if (list_size > 0 && list_size < min_degree) {
-					min_vertex = i;
-					min_degree = list_size;
-				}
-			}
-			return min_vertex;
-		}
-		// Remove a vertex from the adjacency list and matrix
-		void remove_vertex(int vertex) {
-			for (int i = 0; i < test_list[vertex].size(); i++) {
-				int joining_vertex = test_list[vertex].pop_front();
-				std::list<int>::iterator findIter = find(test_list[joining_vertex].begin(), test_list[joining_vertex].end(), vertex);
-				cout << "Value at " << findIter << endl;
-				test_list[joining_vertex].erase(findIter);
-			}
-			/*
-			for(int i = 0; i < num_vertices; i++) {
-				if (adj_matrix[vertex][i] == true){
-					adj_matrix[vertex][i] = false;
-					adj_matrix[i][vertex] = false;
-					adj_list[i].remove(vertex);
-					adj_list[vertex].remove(i);
-				}
-			}
-			*/
-		}
-};
 
-Graph * read_graph() {
+
+void read_graph(multimap<int, int> *adj_map) {
 	int num_nodes;
 	int num_edges;
 	// Open file
-	cout << "before ifstream" <<endl;
 	std::ifstream graphData ("brock800_1.clq");
 	std::string line;
-	Graph *as;
 	// While there is another line
 	while (std::getline(graphData, line))
 	{
@@ -91,35 +21,58 @@ Graph * read_graph() {
 		// If line starts with 'p', initialise as
 		if (line[0] == 'p') {
 			sscanf(line.c_str(), "%*s %*s %d %d", &num_nodes, &num_edges);
-			cout << num_nodes << endl;
-			as = new Graph(num_nodes);
+			cout << "# Vertexes: " << num_nodes << endl;
 		}
 		// Else if line starts with 'e', add to as
 		else if (line[0] == 'e') {
-			cout << "found an edge" << endl;
 			int node1;
 			int node2;
 			sscanf(line.c_str(), "%*s %d %d", &node1, &node2);
-			cout << "after scanf" << endl;
 			
-			as->add_edge(node1, node2);
-			as->add_edge(node1, node2);
+			adj_map->insert(make_pair(node1, node2));
+			adj_map->insert(make_pair(node2, node1));
 		}
 	}
 	cout << "closing graph" << endl;
 	graphData.close();
-	return as;
+	return;
+}
+
+int find_min_degree(multimap<int, int> *adj_map) {
+	return 1;
+}
+
+void remove_vertex(multimap<int, int> *adj_map, int to_remove) {
+	auto ret = adj_map->equal_range(to_remove);
+	cout << "Entered removed vertex" << endl;
+	for (auto it = ret.first; it != ret.second; ++it) {
+		typedef multimap<int, int>::iterator iterator;
+		std::pair<iterator, iterator> iterpair = adj_map->equal_range(it->second);
+		cout << "About to erase" << endl;
+		// Erase (b,15) pair
+		//
+		iterator iter = iterpair.first;
+		for (; iter != iterpair.second; ++it) {
+			if (iter->second == 15) { 
+				adj_map->erase(iter);
+				break;
+				cout << "pair erased" << endl;
+			}
+		}	
+	}
+	cout << "Exiting removed vertex" << endl;
 }
 
 
 int main(int argc, char * argv[]) {
 	list<int> max_independent_set;
-	Graph *as = read_graph();
+	multimap<int, int> adj_map;
+	read_graph(&adj_map);
 	do {
-		int next_vertex = as->find_min_degree();
+		int next_vertex = find_min_degree(&adj_map);
 		if (next_vertex != 0) {
 			max_independent_set.push_back(next_vertex);
-			as->remove_vertex(next_vertex);
+			remove_vertex(&adj_map, next_vertex);
 		} else break;
 	} while (true);
 	cout << "Num vertexs: " << max_independent_set.size() << endl;
